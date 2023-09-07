5fa150dc852372da4b2d17128c3de2c006cf5a25
Kendal Quesada <kendalqb1@gmail.com>
2023-09-07 08:58:42 -0600

    Prueba3

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import './App.css'
 
 function App() {
   const [URL, setURL] = useState('https://www.greetingsisland.com/ecard/pn0kyuzey2oc');
+  const [errorForm, setErrorForm] = useState(false)
+
+
   const [inputValue, setInputValue] = useState('');
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
@@ -14,8 +17,11 @@ function App() {
     console.log('Valor del campo de entrada:', inputValue);
     if (inputValue == 'admin') {
       window.location.href = URL;
+      setErrorForm(false);
     }
-    
+    else {
+      setErrorForm(true)
+    }
   };
 
   return (
@@ -24,27 +30,37 @@ function App() {
       <form onSubmit={handleSubmit}>
         <label style={labelStyle}>
           Contraseña:
-          <input
+          
+        </label>
+        <input
             type="password"
             value={inputValue}
             onChange={handleInputChange}
-            style={inputStyle}
+            style={errorForm ? inputStyleError : inputStyle}
           />
-        </label>
         <button type="submit" style={buttonStyle}>
-          Ir a la carta
+          Ver carta
         </button>
       </form>
     </>
   )
 }
-// Me puedes dar unos estilos mas lindos y minimalistas
-// al label 
+
 const labelStyle = {
   display: 'block',
-  marginBottom: '8px',
+  marginBottom: '15px',
 }
 
+const inputStyleError = {
+  padding: '8px 16px',
+  fontSize: '16px',
+  border: '1px solid #FF0000',
+  borderRadius: '4px',
+  width: '100%',
+  boxSizing: 'border-box',
+  marginBottom: '16px',
+  
+};
 
 const inputStyle = {
   padding: '8px 16px',
